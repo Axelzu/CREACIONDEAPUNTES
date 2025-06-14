abe55e050512c4979233e5967b842d4f4793bfc1
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2025-06-14 16:19:08 -0500

    COMMIT 4

--- CREACIONDEAPUNTES/AboutPage.xaml.cs
+++ CREACIONDEAPUNTES/AboutPage.xaml.cs
@@ -2,10 +2,11 @@ namespace CREACIONDEAPUNTES;
 
 public partial class AboutPage : ContentPage
 {
-	public AboutPage()
-	{
-		
+    public AboutPage()
+    {
+        InitializeComponent();
     }
+
     private async void LearnMore_Clicked(object sender, EventArgs e)
     {
         if (BindingContext is Models.About about)
