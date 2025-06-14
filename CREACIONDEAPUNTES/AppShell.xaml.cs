9c1045ca86b65cd998316453a671630a9478a21b
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2025-06-14 14:36:28 -0500

    Agregar archivos de proyecto.

---
+++ CREACIONDEAPUNTES/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CREACIONDEAPUNTES
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CREACIONDEAPUNTES/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CREACIONDEAPUNTES
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
